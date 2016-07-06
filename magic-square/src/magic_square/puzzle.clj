51c9288a08be88028a58c32db65654295eff1df9
Charlie Hebert <charlie.hebert92@gmail.com>
2016-07-06 15:14:58 -0600

    Get to point where we permute array. Todo: clean up permute and finish puzzle

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -6,3 +6,55 @@
   [[1.0 1.5 2.0]
    [2.5 3.0 3.5]
    [4.0 4.5 5.0]])
+
+(defn generate-3-by-3-vector
+  [[a b c d e f g h i :as values]]
+  {:pre [(= 9 (count values))]}
+  [[a b c]
+   [d e f]
+   [g h i]])
+
+(defn has-3-rows?
+  [test-vector]
+  (= 3 (count test-vector)))
+
+(defn has-3-columns?
+  [test-vector]
+  (let [row-1 (get test-vector 0)
+        row-2 (get test-vector 1)
+        row-3 (get test-vector 2)]
+    (and (= 3 (count row-1))
+         (= 3 (count row-2))
+         (= 3 (count row-3)))))
+
+(defn is-3-by-3-vector?
+  [test-vector]
+  (and (has-3-rows? test-vector)
+       (has-3-columns? test-vector)))
+
+(defn array-size-2?
+  [elems]
+  (= 2 (count elems)))
+
+(defn get-array-less-elem
+  [elem arr]
+  (filterv #(not= elem %) arr))
+
+(defn permute-array
+  [elems]
+  (if (array-size-2? elems)
+    (let [[a b] elems]
+      [[a b]
+       [b a]])
+    (into #{} (map vec (partition (count elems)
+                                 (vec (flatten (mapv (fn [elem]
+                                                       (let [array-less-elem (get-array-less-elem elem elems)]
+                                                         (let [remaining-elems-permutations (permute-array array-less-elem)]
+                                                           (mapv (fn [permutation]
+                                                                   (vec (concat [elem] permutation)))
+                                                                 remaining-elems-permutations))))
+                                                     elems))))))))
+
+(defn permute-3-by-3-vector
+  [test-vector]
+  [])
--- magic-square/test/magic_square/puzzle_test.clj
+++ magic-square/test/magic_square/puzzle_test.clj
@@ -15,12 +15,22 @@
    (+ (get-in m [2 0]) (get-in m [1 1]) (get-in m [0 2]))])
 
 (deftest test-magic-square
-  (testing "all the rows, columns, and diagonal add to the same number"
-    (is (= (set (sum-rows (magic-square values)))
-           (set (sum-cols (magic-square values)))
-           (set (sum-diagonals (magic-square values)))))
+  (testing "it generates a 3 by 3 vector from a list of 9 elems"
+    (let [test-vec (generate-3-by-3-vector values)]
+      (is (is-3-by-3-vector? test-vec))))
+  (testing "permutations"
+    (testing "it generates all permutations of an array"
+      (is (= #{[1 2 3] [1 3 2] [2 1 3] [2 3 1] [3 1 2] [3 2 1]}
+             (permute-array [1 2 3]))))
+    (testing "it generates all permutations of 3 by 3 vector"
+      (is (permute-3-by-3-vector [])))))
 
-    (is (= 1
-           (count (set (sum-rows (magic-square values))))
-           (count (set (sum-cols (magic-square values))))
-           (count (set (sum-diagonals (magic-square values))))))))
+;; (testing "all the rows, columns, and diagonal add to the same number"
+;;     (is (= (set (sum-rows (magic-square values)))
+;;            (set (sum-cols (magic-square values)))
+;;            (set (sum-diagonals (magic-square values)))))
+
+;;     (is (= 1
+;;            (count (set (sum-rows (magic-square values))))
+;;            (count (set (sum-cols (magic-square values))))
+;;            (count (set (sum-diagonals (magic-square values)))))))
