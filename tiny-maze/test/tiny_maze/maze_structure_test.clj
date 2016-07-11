db363caa54e37809dd4d5c83db19932a1694eef2
Charlie Hebert <charlie.hebert92@gmail.com>
2016-07-10 19:45:14 -0600

    Fix bugs in maze structure test

--- tiny-maze/test/tiny_maze/maze_structure_test.clj
+++ tiny-maze/test/tiny_maze/maze_structure_test.clj
@@ -3,16 +3,16 @@
             [tiny-maze.maze-structure :refer :all]))
 (deftest maze-structure
   (testing "columns managed correctly"
-    (is true? (equal-number-of-columns? [[1 1 1]
-                                         [1 1 1]
-                                         [1 1 1]]))
-    (is false? (equal-number-of-columns? [[1 1]
+    (is (true? (equal-number-of-columns? [[1 1 1]
                                           [1 1 1]
                                           [1 1 1]])))
+    (is (false? (equal-number-of-columns? [[1 1]
+                                           [1 1 1]
+                                           [1 1 1]]))))
   (testing "square maze correctly identified"
     (is (is-square-maze? [[1 1 1]
                           [1 1 1]
                           [1 1 1]]))
-    (is false? (is-square-maze? [[1 1 1]
-                                 [1 1]
-                                 [1 1 1]]))))
+    (is (false? (is-square-maze? [[1 1 1]
+                                  [1 1]
+                                  [1 1 1]])))))
