8278bbc231b4d0224cd752b3f4835be0662f9c18
Charlie Hebert <charlie.hebert92@gmail.com>
2016-07-15 18:56:44 -0600

    Get winning card from cards

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,9 +1,43 @@
-(ns card-game-war.game)
+(ns card-game-war.game
+  (:require [card-game-war.card-query :as c-q]))
 
 (defn shuffle-deck
   [deck]
   (shuffle deck))
 
-(defn play-round [player1-card player2-card])
+(defn card-beats-all-cards?
+  [cards card-to-check]
+  (let [card-loses? (complement c-q/card-wins?)
+        cards-that-beat-card (filter (fn [card]
+                                       (card-loses? card-to-check card))
+                                     cards)]
+    (= 0 (count cards-that-beat-card))))
+
+(defn card-in-cards?
+  [cards card]
+  (let [card-index (.indexOf cards card)]
+    (>= card-index 0)))
+
+(defn remove-card-from-cards
+  [cards card-to-remove]
+  {:pre [(card-in-cards? cards card-to-remove)]}
+  (filterv (fn [card] (not= card card-to-remove))
+           cards))
+
+(defn get-winning-card
+  [cards]
+  (let [results (mapv (fn [card]
+                        (let [cards-less-card (remove-card-from-cards cards
+                                                                      card)]
+                          (card-beats-all-cards? cards-less-card card)))
+                      cards)
+        index-of-winner (.indexOf results true)
+        winning-card (get cards index-of-winner)]
+    winning-card))
+
+(defn play-round
+  [player-hands]
+  (let [cards (mapv first player-hands)]
+    cards))
 
 (defn play-game [player1-cards player2-cards])
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -4,13 +4,20 @@
             [card-game-war.cards :refer :all]))
 
 (deftest test-play-round
-  (testing "the highest rank wins the cards in the round")
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+  (testing "Correctly removes card from hand of cards"
+    (is (= [ace-of-spades two-of-spades]
+           (remove-card-from-cards [ace-of-spades two-of-hearts two-of-spades]
+                                   two-of-hearts)))
+    (is (thrown? java.lang.AssertionError
+                 (remove-card-from-cards [ace-of-spades two-of-hearts]
+                                         :non-existent))))
+  (testing "Gets winning card from cards"
+    (is (= ace-of-hearts
+           (get-winning-card [ace-of-spades ace-of-hearts two-of-hearts
+                              two-of-spades]))))
+  #_(testing "Winner has other players' cards placed at the bottom of his deck"
+    (is (= [[] [two-of-spades ace-of-spades]]
+           (play-round [[ace-of-spades] [two-of-spades]])))))
 
 (deftest test-play-game
   (testing "the player loses when they run out of cards"))
